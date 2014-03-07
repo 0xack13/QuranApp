913878ecdea61779d5899cf02d3fe0691ec40464
0xack13 <0xack13@gmail.com>
2014-03-07 20:29:19 +0300

    Initial Commit

---
+++ QuranApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QuranApp
+//
+//  Created by 0xack13 on 3/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QuranAppTests/QuranAppTests.m
@@ -0,0 +1,34 @@
+//
+//  QuranAppTests.m
+//  QuranAppTests
+//
+//  Created by 0xack13 on 3/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuranAppTests : XCTestCase
+
+@end
+
+@implementation QuranAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
